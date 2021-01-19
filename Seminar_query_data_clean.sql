4fdefec58865011d39f262021de375a64e61c769
thomasss99 <thomas.brink@upcmail.nl>
2021-01-19 17:27:58 +0100

    Update Seminar_query_data_clean.sql

--- Seminar_query_data_clean.sql
+++ Seminar_query_data_clean.sql
@@ -1,6 +1,8 @@
 use Seminar;
 
 DROP TABLE bol_data;
+DROP TABLE data_2019;
+DROP TABLE data_2020;
 
 SELECT *
 INTO bol_data
@@ -14,6 +16,8 @@ ALTER COLUMN totalPrice FLOAT;
 
 -- Original data set -> 4.779.466 rows
 SELECT COUNT(*) FROM bol_data;
+SELECT COUNT(*) FROM cleaned_bol_data;
+SELECT DISTINCT * FROM cleaned_bol_data;
 
 -- Remove duplicate rows from the table -> 4.454.130 rows
 SELECT DISTINCT * 
@@ -33,7 +37,7 @@ SELECT *
 INTO cleaned_bol_data -- cleaned table
 FROM bol_data;
 
--- Delete all noise (non-sensible rows) -> delete 5.577 rows
+-- Delete all noise (non-sensible rows) -> delete 6.516 rows
 DELETE FROM cleaned_bol_data
 WHERE ((startDateCase < orderDate) OR (cancellationDate < orderDate) OR (promisedDeliveryDate < orderDate)
 		OR (shipmentDate < orderDate) OR (CONVERT(date,dateTimeFirstDeliveryMoment) < orderDate) OR (returnDateTime < orderDate)
@@ -41,9 +45,10 @@ WHERE ((startDateCase < orderDate) OR (cancellationDate < orderDate) OR (promise
 		OR (cancellationDate > returnDateTime) OR (returnDateTime < (CONVERT(date,dateTimeFirstDeliveryMoment)) 
 		AND dateTimeFirstDeliveryMoment IS NOT NULL AND returnDateTime IS NOT NULL) OR (shipmentDate > returnDateTime)
 		OR (shipmentDate > CONVERT(date,dateTimeFirstDeliveryMoment)) OR (registrationDateSeller IS NULL)
+		OR (promisedDeliveryDate IS NULL) 
 		OR (cancellationDate > shipmentDate AND (cancellationReasonCode = 'CUST_FE' OR cancellationReasonCode = 'CUST_CS')));
 
--- Count the rows in the resulting, cleaned table -> 4.773.889 rows
+-- Count the rows in the resulting, cleaned table -> 4.772.950 rows
 SELECT COUNT(*) FROM cleaned_bol_data;
 
 -- CHECKS
@@ -77,12 +82,13 @@ SELECT diffReturnCheck, diffCaseCheck, diffCancellationCheck, diffOnTimeDelivery
 FROM diff_return_check
 GROUP BY diffReturnCheck, diffCaseCheck, diffCancellationCheck, diffOnTimeDeliveryCheck;
 
--- All entries with a cancellation reason for which the noCancellation boolean is 1 (51.961 rows)
+-- All entries with a cancellation reason for which the noCancellation boolean is 1 (51.579 rows)
 SELECT *
 FROM cleaned_bol_data
 WHERE cancellationReasonCode IS NOT NULL 
 	AND noCancellation = 1
 	AND DATEDIFF(DAY,orderDate,cancellationDate) <= 10;
+
 SELECT *
 FROM cleaned_bol_data
 WHERE noCancellation = 0;
@@ -152,29 +158,29 @@ SELECT	cbd.*,
 				WHEN (noCancellation = 0)
 					THEN 'Cancellation'
 		END AS determinantClassification,
-		@looking_forward_days as lookingForwardDays, -- adjust yourself 
-		CASE	WHEN (DATEDIFF(day,orderDate,CONVERT(DATE,dateTimeFirstDeliveryMoment)) < @looking_forward_days 
-				  AND DATEDIFF(day,promisedDeliveryDate,CONVERT(DATE,dateTimeFirstDeliveryMoment)) <= 0)
-					THEN 'On time'
-				WHEN (DATEDIFF(day,orderDate,CONVERT(DATE,dateTimeFirstDeliveryMoment)) < @looking_forward_days
-				  AND DATEDIFF(day,promisedDeliveryDate,CONVERT(DATE,dateTimeFirstDeliveryMoment)) > 0)
-					THEN 'Late'
-				ELSE 'Unknown'
-		END AS deliveryCategory,
-		CASE	WHEN (DATEDIFF(day,orderDate,CONVERT(DATE,startDateCase)) < @looking_forward_days)
-					THEN 'Case started'
-				ELSE 'Case not (yet) started'
-		END AS caseCategory,
-		CASE	WHEN (DATEDIFF(day,orderDate,returnDateTime) < @looking_forward_days)
-					THEN 'Delivered, returned'
-				WHEN (DATEDIFF(day,orderDate,CONVERT(DATE,dateTimeFirstDeliveryMoment)) < @looking_forward_days)
-					THEN 'Delivered, not (yet) returned'
-				ELSE 'Not yet delivered'
-		END AS returnCategory,
+		--@looking_forward_days as lookingForwardDays, -- adjust yourself 
+		--CASE	WHEN (DATEDIFF(day,orderDate,CONVERT(DATE,dateTimeFirstDeliveryMoment)) < @looking_forward_days 
+		--		  AND DATEDIFF(day,promisedDeliveryDate,CONVERT(DATE,dateTimeFirstDeliveryMoment)) <= 0)
+		--			THEN 'On time'
+		--		WHEN (DATEDIFF(day,orderDate,CONVERT(DATE,dateTimeFirstDeliveryMoment)) < @looking_forward_days
+		--		  AND DATEDIFF(day,promisedDeliveryDate,CONVERT(DATE,dateTimeFirstDeliveryMoment)) > 0)
+		--			THEN 'Late'
+		--		ELSE 'Unknown'
+		--END AS deliveryCategory,
+		--CASE	WHEN (DATEDIFF(day,orderDate,CONVERT(DATE,startDateCase)) < @looking_forward_days)
+		--			THEN 'Case started'
+		--		ELSE 'Case not (yet) started'
+		--END AS caseCategory,
+		--CASE	WHEN (DATEDIFF(day,orderDate,returnDateTime) < @looking_forward_days)
+		--			THEN 'Delivered, returned'
+		--		WHEN (DATEDIFF(day,orderDate,CONVERT(DATE,dateTimeFirstDeliveryMoment)) < @looking_forward_days)
+		--			THEN 'Delivered, not (yet) returned'
+		--		ELSE 'Not yet delivered'
+		--END AS returnCategory,
 		DATEPART(year,orderDate) AS orderYear,
 		FORMAT(DATEPART(month,orderDate),'00') AS orderMonth,
 		CONCAT(DATEPART(year,orderDate),'-',FORMAT(DATEPART(month,orderDate),'00')) AS orderYearMonth,
-		DATEPART(weekday,orderDate) as orderWeekday,
+		DATEPART(weekday,orderDate) AS orderWeekday,
 		CASE	WHEN DATEPART(weekday,orderDate) <= 5
 					THEN 0
 				ELSE 1
@@ -193,7 +199,31 @@ SELECT	cbd.*,
 		DATEDIFF(day,orderDate,promisedDeliveryDate) AS promisedDeliveryDays,
 		DATEDIFF(day,orderDate,dateTimeFirstDeliveryMoment) AS actualDeliveryDays,
 		DATEDIFF(day,orderDate,startDateCase) AS caseDays,
-		DATEDIFF(day,orderDate,returnDateTime) AS returnDays
+		DATEDIFF(day,orderDate,returnDateTime) AS returnDays,
+		CASE WHEN countryCode = 'NL' THEN 1 ELSE 0 END AS countryCodeNL,
+		CASE WHEN fulfilmentType = 'FBB' THEN 1 ELSE 0 END AS fulfilmentByBol,
+		CASE WHEN countryOriginSeller = 'NL' THEN 1 ELSE 0 END AS countryOriginNL,
+		CASE WHEN countryOriginSeller = 'BE' THEN 1 ELSE 0 END AS countryOriginBE,
+		CASE WHEN countryOriginSeller = 'DE' THEN 1 ELSE 0 END AS countryOriginDE,
+		CASE WHEN DATEPART(weekday,orderDate) = 1 THEN 1 ELSE 0 END AS orderMonday,
+		CASE WHEN DATEPART(weekday,orderDate) = 2 THEN 1 ELSE 0 END AS orderTuesday,
+		CASE WHEN DATEPART(weekday,orderDate) = 3 THEN 1 ELSE 0 END AS orderWednesday,
+		CASE WHEN DATEPART(weekday,orderDate) = 4 THEN 1 ELSE 0 END AS orderThursday,
+		CASE WHEN DATEPART(weekday,orderDate) = 5 THEN 1 ELSE 0 END AS orderFriday,
+		CASE WHEN DATEPART(weekday,orderDate) = 6 THEN 1 ELSE 0 END AS orderSaturday,
+		CASE WHEN DATEPART(weekday,orderDate) = 7 THEN 1 ELSE 0 END AS orderSunday,
+		CASE WHEN FORMAT(DATEPART(month,orderDate),'00') = '01' THEN 1 ELSE 0 END AS orderJanuary,
+		CASE WHEN FORMAT(DATEPART(month,orderDate),'00') = '02' THEN 1 ELSE 0 END AS orderFebruary,
+		CASE WHEN FORMAT(DATEPART(month,orderDate),'00') = '03' THEN 1 ELSE 0 END AS orderMarch,
+		CASE WHEN FORMAT(DATEPART(month,orderDate),'00') = '04' THEN 1 ELSE 0 END AS orderApril,
+		CASE WHEN FORMAT(DATEPART(month,orderDate),'00') = '05' THEN 1 ELSE 0 END AS orderMay,
+		CASE WHEN FORMAT(DATEPART(month,orderDate),'00') = '06' THEN 1 ELSE 0 END AS orderJune,
+		CASE WHEN FORMAT(DATEPART(month,orderDate),'00') = '07' THEN 1 ELSE 0 END AS orderJuly,
+		CASE WHEN FORMAT(DATEPART(month,orderDate),'00') = '08' THEN 1 ELSE 0 END AS orderAugust,
+		CASE WHEN FORMAT(DATEPART(month,orderDate),'00') = '09' THEN 1 ELSE 0 END AS orderSeptember,
+		CASE WHEN FORMAT(DATEPART(month,orderDate),'00') = '10' THEN 1 ELSE 0 END AS orderOctober,
+		CASE WHEN FORMAT(DATEPART(month,orderDate),'00') = '11' THEN 1 ELSE 0 END AS orderNovember,
+		CASE WHEN FORMAT(DATEPART(month,orderDate),'00') = '12' THEN 1 ELSE 0 END AS orderDecember
 INTO cleaned_bol_data_full
 FROM cleaned_bol_data as cbd
 LEFT JOIN transporter_classification as tc
@@ -263,4 +293,19 @@ GROUP BY generalMatchClassification, noCancellation;
 SELECT *
 FROM cleaned_bol_data
 WHERE promisedDeliveryDate IS NULL;
+
+SELECT currentCountryAvailabilitySeller, COUNT(*)
+FROM cleaned_bol_data
+GROUP BY currentCountryAvailabilitySeller;
+
+SELECT sellerId, COUNT(DISTINCT currentCountryAvailabilitySeller)
+FROM cleaned_bol_data
+GROUP BY sellerId
+ORDER BY COUNT(DISTINCT currentCountryAvailabilitySeller) DESC;
+
+SELECT * FROM cleaned_bol_data
+WHERE quantityReturned > quantityOrdered;
+
+SELECT * FROM cleaned_bol_data
+WHERE noReturn = 0;
 
